825eea83ce3c9863891a7fc852d67c850e7bf27d
Sam <orgillsams@gmail.com>
2016-11-24 21:51:36 +0000

    Updates

--- app/models/user.js
+++ app/models/user.js
@@ -4,7 +4,8 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var UserSchema   = new Schema({
-    name: String
+    name: String,
+    password: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
